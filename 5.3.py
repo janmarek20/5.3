bb913cfb33b99ad70b166bdd14a09f534b998890
Jan Marek <janekmvi@gmail.com>
2024-05-14 00:10:27 +0200

    Name change (label_length), contact() changes, Initialization before loop, create_contacts exists

--- 5.3.py
+++ 5.3.py
@@ -14,7 +14,7 @@ class BaseContact:
         return f'Wybieram numer {self.phone} i dzownię do {self.name}'
     
     @property
-    def name_length(self):
+    def label_length(self):
         return len(self.name) + len(self.surname)
 
 
@@ -25,36 +25,24 @@ class BusinessContact(BaseContact):
         self.company = company
         self.company_phone = company_phone
 
-    def __str__(self):
-        return super().__str__()
-
     def contact(self):
-        return super().contact()
-
-    def name_length(self):
-        return super().name_length
+        return f'Wybieram numer {self.phone} i dzownię do {self.name}'
 
 def create_contact(id_type, amount):
     ids_list = []
     if id_type == 1:
+        fake_data = Faker()
         for i in range(amount):
-            fake_data = Faker()
             fake_data_list = [fake_data.name().split(' ')[0], fake_data.name().split(' ')[1], fake_data.phone_number(), fake_data.email()]
             fake_inf = BaseContact(*fake_data_list)
-            print(fake_inf)
-            print(fake_inf.contact())
-            print(fake_inf.name_length)
             ids_list.append(fake_inf)
 
             
     elif id_type == 2:
+        fake_data = Faker()
         for i in range(amount):
-            fake_data = Faker()
             fake_data_list = [fake_data.job(), fake_data.company(), fake_data.phone_number(), fake_data.name().split(' ')[0], fake_data.name().split(' ')[1], fake_data.phone_number(), fake_data.email()]
             fake_inf = BusinessContact(*fake_data_list)
-            print(fake_inf)
-            print(fake_inf.contact())
-            print(fake_inf.name_length())
             ids_list.append(fake_inf)
     return ids_list
 
@@ -64,3 +52,7 @@ if __name__ == "__main__":
     cards_type = int(input('Podaj rodzaj wizytówki: \n 1: base \n 2: business \n'))
     cards_amount = int(input('Podaj liczbę wizytówek: '))
     contacts = create_contact(cards_type, cards_amount)
+    for contact in contacts:
+        print(contact)
+        print(contact.contact())
+        print(contact.label_length)
