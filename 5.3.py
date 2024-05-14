8670c1b9810694d88aea5ae928e4f39eab85126e
Jan Marek <janekmvi@gmail.com>
2024-05-14 19:34:10 +0200

    Changes after review

--- 5.3.py
+++ 5.3.py
@@ -6,16 +6,17 @@ class BaseContact:
         self.surname = surname
         self.phone = phone
         self.mail = mail
+        self.__label_length = len(self.name) + len(self.surname)
 
     def __str__(self):
         return f'{self.name} {self.surname}'
     
     def contact(self):
-        return f'Wybieram numer {self.phone} i dzownię do {self.name}'
+        return f'Wybieram numer {self.phone} i dzwonię do {self.name}'
     
     @property
     def label_length(self):
-        return len(self.name) + len(self.surname)
+        return self.__label_length
 
 
 class BusinessContact(BaseContact):
@@ -26,12 +27,12 @@ class BusinessContact(BaseContact):
         self.company_phone = company_phone
 
     def contact(self):
-        return f'Wybieram numer {self.phone} i dzownię do {self.name}'
+        return f'Wybieram numer {self.company_phone} i dzwonię do {self.name}'
 
 def create_contact(id_type, amount):
     ids_list = []
+    fake_data = Faker()
     if id_type == 1:
-        fake_data = Faker()
         for i in range(amount):
             fake_data_list = [fake_data.name().split(' ')[0], fake_data.name().split(' ')[1], fake_data.phone_number(), fake_data.email()]
             fake_inf = BaseContact(*fake_data_list)
@@ -39,7 +40,6 @@ def create_contact(id_type, amount):
 
             
     elif id_type == 2:
-        fake_data = Faker()
         for i in range(amount):
             fake_data_list = [fake_data.job(), fake_data.company(), fake_data.phone_number(), fake_data.name().split(' ')[0], fake_data.name().split(' ')[1], fake_data.phone_number(), fake_data.email()]
             fake_inf = BusinessContact(*fake_data_list)
